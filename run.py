deda3dbb6c004e21ee353a20c62755721463520c
Alisa Gao <122762285+3374128044@users.noreply.github.com>
2024-12-09 22:51:16 -0800

    use redis for session management

--- run.py
+++ run.py
@@ -8,9 +8,5 @@ with app.app_context():
     
 print(app.url_map)
 
-@app.route('/test',methods=['GET'])
-def test():
-    return jsonify({"message":"success"}),200
-
 if __name__ == "__main__":
     app.run(debug=True)
