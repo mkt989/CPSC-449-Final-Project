7eb20c3ad11d82c3da8be20f108f95009a8b1b9a
mkt989 <122516715+mkt989@users.noreply.github.com>
2024-12-11 04:11:58 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", 'sqlite:///recipes.db') #temporary
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = True
